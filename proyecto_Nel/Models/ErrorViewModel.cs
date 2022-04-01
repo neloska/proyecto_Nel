4d051b74aea361c2e07951e80f285fb4dfb496ce
neloska <neloskasaxo@gmail.com>
2022-04-01 12:58:21 +0200

    Agregar archivos de proyecto.

---
+++ proyecto_Nel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proyecto_Nel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
